009dccefa31fe936b55c1ac5e94b9dc594ac16b7
Eqbal HNZ <e.hassannejad@protonmail.com>
2024-07-25 00:35:31 +0330

    solved question 4

---
+++ 1-simple-programs/4.py
@@ -0,0 +1,22 @@
+def get_number(index):
+
+    try:
+        return int(input(f"Please enter number {index}: "))
+    except ValueError:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        return get_number(index)
+
+def calc_avg(a, b, c):
+
+    return (a + b + c) / 3
+
+def main():
+
+   a = get_number(1) 
+   b = get_number(2)
+   c = get_number(3)
+   avg = calc_avg(a, b, c)
+   print(f"({a} + {b} + {c}) / 3 = {avg}")
+
+if __name__ == "__main__":
+    main()
