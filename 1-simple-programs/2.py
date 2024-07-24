963afe3e1ce65e52eb341684fa361e3d5329d0de
Eqbal HNZ <e.hassannejad@protonmail.com>
2024-07-24 19:59:05 +0330

    Solved exercise number 2

--- 1-simple-programs/2.py
+++ 1-simple-programs/2.py
@@ -13,18 +13,24 @@ def get_input():
     try:
         r = int(input("Please Enter Radius: "))
         if r <= 0:
-            print("A circle can't have radius of {r}! please try again!")
-            get_input()
+            os.system('cls' if os.name == "nt" else "clear")
+            print(f"A circle can't have radius of {r}! please try again!")
+            return get_input()
         else:
             return r
     
-    except(ValueError):
-        os.system('cls' if os.name == "clear" else "cls")
-        print(f"'{r}' is not a number, please try again!")
-        get_input()
+    except ValueError:
+        os.system('cls' if os.name == "nt" else "clear")
+        print(f"That's not a number, please try again!")
+        return get_input()
 
 def main():
-    ...
+
+    radius = get_input() 
+    area = solve_area(radius)
+    perimeter = solve_perimeter(radius)
+
+    print(f"Perimeter: {perimeter}\nArea: {area}")
 
 if __name__ == "__main__":
     main()
