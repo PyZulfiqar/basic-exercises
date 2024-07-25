9445b612c3a00270aa114fa6d7bcf668d89287bb
Eqbal HNZ <e.hassannejad@protonmail.com>
2024-07-25 23:35:16 +0330

    solved exercise 23

---
+++ 3-loops/23.py
@@ -0,0 +1,31 @@
+def get_number(value):
+
+    try:
+        return int(input(f"Please enter {value} Integer: "))
+    except ValueError:
+        print("That's not an integer, Try again!")
+        return get_number(value)
+
+
+def find_cd(a, b):
+
+    # a is considered smaller than be in the function
+    print("Common Divisors: ", end='')
+    for i in range(1, a + 1):
+        if a % i == 0 and b % i == 0:
+            print(i, end=' ')
+    print()
+
+
+def main():
+
+    x = get_number("first")
+    y = get_number("second")
+
+    if x <= y:
+        find_cd(x, y)
+    else:
+        find_cd(y, x)
+
+if __name__ == "__main__":
+    main()
