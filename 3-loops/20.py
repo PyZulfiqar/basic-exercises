b69ec7c9f1a73ab7dd7f0b545c20e955f73ced8e
Eqbal HNZ <e.hassannejad@protonmail.com>
2024-07-25 23:00:27 +0330

    solved exercise 20

---
+++ 3-loops/20.py
@@ -0,0 +1,21 @@
+def get_input():
+
+    try:
+        return int(input("Please enter an Integer: "))
+    except ValueError:
+        print("That's not an integer, Try again!")
+        return get_input()
+
+number = get_input()
+
+if number == 0:
+    print("0 doesn't have any divisors")
+    exit()
+
+if number < 0:
+    number = number * -1
+
+for i in range (1, number + 1):
+    if number % i == 0:
+        print(i, end=' ')
+print()
