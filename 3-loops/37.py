9a26645ee2ad1b20648fb56d877e6d2db66b35c1
Eqbal HNZ <e.hassannejad@protonmail.com>
2024-07-26 21:15:27 +0330

    solved 37

---
+++ 3-loops/37.py
@@ -0,0 +1,42 @@
+def get_avg_grade(number):
+
+    try:
+        avg_grade = float(input(f"Enter student {number}'s average grade: "))
+        if avg_grade < 0:
+            print(f"An average grade can't be a negative number, Try again.")
+            return get_avg_grade(number)
+        elif avg_grade > 20:
+            print(f"An average grade can't be a more than 20, Try again.")
+            return get_avg_grade(number)
+        else:
+            return avg_grade
+    except ValueError:
+        print("That's not a number, Try again.")
+        return get_avg_grade(number)
+
+def check_max_avg(max_avg, new_avg):
+
+    if new_avg >= max_avg:
+        return new_avg
+    else:
+        return max_avg
+
+def check_academic_probation(avg):
+
+    if avg < 10:
+        return True
+    else:
+        False
+
+def main():
+
+    max_avg = 0
+    for i in range(1, 101):
+        new_avg = get_avg_grade(i)
+        if check_academic_probation(new_avg):
+            max_avg = check_max_avg(max_avg, new_avg) 
+
+    print(f"The highest grade average among students on academic probation: {max_avg}")
+
+if __name__ == "__main__":
+    main()
