0b2656ffaeed413b84a936643dcee17c8ea866f5
Eqbal HNZ <e.hassannejad@protonmail.com>
2024-07-28 12:36:44 +0330

    added colors to 49.py for terminal

--- 3-loops/49.py
+++ 3-loops/49.py
@@ -2,21 +2,25 @@ import os
 
 def get_number():
     try:
-        foo = float(input("Enter a number between -1 and +1: "))
+        foo = float(input("\033[92m\x1b[1mEnter a number between -1 and +1:\x1b[0m "))
         if 0 <= abs(foo) < 1:
             os.system('cls' if os.name == 'nt' else 'clear')
             return foo
         else: 
             os.system('cls' if os.name == 'nt' else 'clear')
-            print("Invalid number, Try again")
+            print("\x1b[31m\x1b[1mInvalid number, Try again")
             return get_number()
     except ValueError:
         os.system('cls' if os.name == 'nt' else 'clear')
-        print("Invalid input, Try again")
+        print("\x1b[31m\x1b[1mInvalid input, Try again")
         return get_number()
 
 def main():
 
+    # enable ansii escape characters for windows
+    if os.name == 'nt':
+        os.system('') 
+    
     os.system('cls' if os.name == 'nt' else 'clear')
 
     c = 0
@@ -31,7 +35,7 @@ def main():
     
     avg = sum_pos / c if c > 0 else 'No positive numbers' 
 
-    print(f"The average of the positive numbers: {avg}")
+    print(f"\033[92m\x1b[1mThe average of the positive numbers: \x1b[30m\x1b[47m{avg}\x1b[0m")
 
 if __name__ == "__main__":
     main()
