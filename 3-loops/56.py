c4c2ce65b6e0c72c8b5cd5d03ede082d4803f910
Eqbal HNZ <e.hassannejad@protonmail.com>
2024-07-30 19:14:41 +0330

    solved 56

--- 3-loops/56.py
+++ 3-loops/56.py
@@ -7,10 +7,26 @@ ansii = {
     "underline": "\x1b[4m\x1b[1m"
 }
 
+def convert_base(x, b):
+
+
+    tmp = "" 
+    while True:
+        r = x % b
+        x = x // b
+        if x < b:
+            tmp = str(r) + tmp
+            tmp = str(x) + tmp
+            break
+        else:
+            tmp = str(r) + tmp
+    
+    return tmp
+
 def get_number(v):
 
     try:
-        n = int(input(f"{ansii['green']}Enter {v}{ansii['reset']}")) 
+        n = int(input(f"{ansii['green']}Enter {v}: {ansii['reset']}")) 
         if n < 1:
             print(f"{ansii['red']}Invalid number, Try again.{ansii['reset']}")
             return get_number(v)
@@ -18,7 +34,7 @@ def get_number(v):
             if v == 'x':
                 return n
             else:
-                if n > 10:
+                if n >= 10 or n == 1:
                     print(f"{ansii['red']}Invalid number, Try again.{ansii['reset']}")
                     return get_number(v)
                 else:
@@ -31,6 +47,9 @@ def main():
     
     x = get_number('x')
     b = get_number('b')
+    foo =convert_base(x, b)
+
+    print(f"{ansii['green']}{x} in base {b} is: {foo}{ansii['reset']}")
 
 if __name__ == "__main__":
     main()
