c72ed74e85505198cb4d4941868d1c8e11354340
Eqbal HNZ <e.hassannejad@protonmail.com>
2024-08-05 22:05:24 +0330

    solved 74.py

---
+++ 5-arrays/74.py
@@ -0,0 +1,43 @@
+import os
+
+def get_number(v: int) -> float:
+
+    while True:
+        try:
+            n = float(input(f"Enter number {v}: ")) 
+            os.system('cls' if os.name == 'nt' else 'clear')
+            return n
+        except ValueError:
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print("Invalid Input.")
+
+def bubble_sort(arr: list[float]) -> list[float]:
+
+    for i in range(0, len(arr)):
+        for j in range(0, len(arr) - 1):
+            if arr[j + 1] < arr[j]:
+                tmp = arr[j]
+                arr[j] = arr[j + 1]
+                arr[j + 1] = tmp
+    
+    return arr
+
+
+def main():
+
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+    my_arr: list[float] = [0 for i in range(50)] 
+
+    for i in range(0, len(my_arr)):
+        my_arr[i] = get_number(i + 1)
+
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+    sorted_arr = bubble_sort(my_arr)
+
+    print(sorted_arr[25])    
+
+if __name__ == "__main__":
+    main()
